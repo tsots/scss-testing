cbbd17c136933aee2ccee6871a6965ad5b88a538
tsots <mosalatsotetsi@gmail.com>
2025-07-27 02:18:12 +0200

    prepend routes including in the navbar again

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import { Route, Routes } from "react-router";
 import Home from "./routes/Home";
 import About from "./routes/About";
@@ -9,15 +6,14 @@ import Navbar from "./components/Navbar";
 // import "./App.scss";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/scss-testing" exact component={Home} />
-        <Route path="/scss-testing/about" component={About} />
-        <Route path="/scss-testing/contact" component={Contact} />
+        <Route path="/scss-testing" exact element={<Home />} />
+        <Route path="/scss-testing/about" element={<About />} />
+        <Route path="/scss-testing/contact" element={<Contact />} />
       </Routes>
     </>
   );
